091e789bd84669835479818515a040272568c707
Enmanuel D Becerra C <becerracenmanueld@gmail.com>
2025-11-05 10:48:48 -0400

    📌 Nodepp-Redis | Bug Fixing | V1.0.0 📌

--- example/main.cpp
+++ example/main.cpp
@@ -1,16 +1,29 @@
+
 #include <nodepp/nodepp.h>
-#include <redis.h>
+#include <redis/redis.h>
 
 using namespace nodepp;
 
 void onMain() {
 
-    mariadb_t db ("db://auth@localhost:8000");
+    auto db = redis::add("db://localhost:6379");
 
-    db.exec(R"( SET FOO BAT )");
+    for( auto x=100; x-->0; ){ process::add([=](){
+         auto db = redis::add("db://localhost:6379");
 
-    db.exec("GET FOO",[]( string_t data ){
-        console::log( "->", data )
-    });
+         db.emit("LRANGE FOO 0 -1",[=]( string_t data ){
+             console::log( "->", x, data );
+         }); return -1;
+
+    }); }
+
+    db.await( R"( MULTI
+        DEL   FOO
+        LPUSH FOO 1
+        LPUSH FOO 2
+        LPUSH FOO 3
+        LPUSH FOO 4
+        LPUSH FOO 5
+    EXEC )" );
 
 }
